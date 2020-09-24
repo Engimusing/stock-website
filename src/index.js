e08194e9793e7c220423724d1d08881319ecfb0e
Megan <megan.miller@smoldev.dev>
2020-09-24 14:18:38 -0500

    Added Axios for URL request handling.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from "redux"
+import { reducer } from './reducers'
 
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// import {} from '../actions'
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SOME_ACTION:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
