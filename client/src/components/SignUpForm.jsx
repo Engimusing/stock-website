261e3618bc423a96c63012cc3ac87fe9df406a52
Megan <megan.miller@smoldev.dev>
2020-09-29 17:07:00 -0500

    Registration form now works, just have to add a post call to the backend.

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function SignUpForm() {
+    const [ formInput, setFormInput ] = useState({});
+
+    const changeInput = (e) => {
+        setFormInput({...formInput, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(formInput)
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username" placeholder="username"/>
+                <input onChange={changeInput} type="text" name="username" placeholder="username"/>
 
                 <label htmlFor="password">Password: </label>
-                <input type="password" name="password"/>
+                <input onChange={changeInput} type="password" name="password"/>
+
+                <button type="submit">Submit</button>
             </form>        
         </div>
     )
