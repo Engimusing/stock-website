41029ac94ecac019fb1546aa54c2a635089d1ca7
Megan <megan.miller@smoldev.dev>
2020-09-30 16:26:17 -0500

    Updating the codebase to use github actions to deploy to heroku.

--- api/data/migrations/20200928150639_users.js
+++ api/data/migrations/20200928150639_users.js
@@ -5,6 +5,18 @@ exports.up = function(knex) {
       tbl.text("username").unique().notNullable()
       tbl.text("password").notNullable()
       tbl.text("name")
+  }).createTable("status", tbl => {
+    tbl.increments("id")
+    tbl.string("ticker")
+    tbl.float("delta")
+    tbl.float("profit")
+    tbl.float("current_price")
+    tbl.float("orig_price")
+    tbl.float("quantity")
+    tbl.float("mark")
+    tbl.string("strike_str")
+    tbl.integer("order_id")
+    tbl.timestamp("time_run").defaultTo(knex.fn.now(6))
   })
 };
 
